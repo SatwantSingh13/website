e396e109b0476677df8b9ff16b51257fcded9a05
NexBid <satwant@nexbid.uk>
2026-07-14 14:16:49 +0530

    Point permanent loader to tracking-safe build

--- nbx/v1.js
+++ nbx/v1.js
@@ -113,7 +113,7 @@
     }
 
     var playerUrl = new URL("player-v1.js", currentScriptUrl);
-    playerUrl.searchParams.set("v", "20260714-1");
+    playerUrl.searchParams.set("v", "20260714-2");
     var playerScript = document.createElement("script");
     playerScript.async = true;
     playerScript.src = playerUrl.toString();
